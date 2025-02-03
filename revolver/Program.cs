3c57d6f99dbda305a25d2f127d45e12ea7fccce6
Changjiang Yang <changjiangyang@noahmed.com>
2025-02-03 14:25:54 -0500

    add usage

--- revolver/Program.cs
+++ revolver/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
 using System.Text;
 using System.Threading.Tasks;
+using Concurrent;
 
 namespace revolver
 {
-    class Program
+    public class BaseClassWithSafeHandle : IDisposable
+    {
+        // To detect redundant calls
+        private bool _disposedValue;
+
+        // Instantiate a SafeHandle instance.
+        private SafeHandle _safeHandle = new SafeFileHandle(IntPtr.Zero, true);
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _safeHandle?.Dispose();
+                    _safeHandle = null;
+                }
+
+                _disposedValue = true;
+            }
+        }
+    }
+
+    internal class Program
     {
         static void Main(string[] args)
         {
+            var rev = new Revolver<BaseClassWithSafeHandle>(1);
+
         }
     }
 }
